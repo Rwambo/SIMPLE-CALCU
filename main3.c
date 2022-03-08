495a5b9b6fca543dd4a073dee081fe7a5f6375e5
Rwambo <88381936+Rwambo@users.noreply.github.com>
2022-03-08 13:50:34 +0300

    Add files via upload

---
+++ main3.c
@@ -0,0 +1,37 @@
+/*
+   Project: county lib management
+   Author: Rwambo
+   Date: Feb, 2022
+   Compiler: C99
+   License: MIT
+*/   
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+ {	
+ 	printf("\rCounty Library\n");
+ 	printf("Welcome Mr. Rwambo. \n");
+ 	printf("What would you like to do?\n");
+ 	printf("Menu\n");
+ 	printf("\1. View users\n");
+ 	printf("\2. Add new User\n");
+ 	printf("\3. Edit user\n");
+ 	printf("\4. Delete user\n");
+ 	printf("\5. Change Password\n");
+ 	printf("\6. Log out\n");
+ 	printf("\7. Exit system\n");
+ 	
+ 	int menu;
+ 	
+ 	
+ 	// Execute action
+ 	if("1, show list of all users");
+ 	if("2, allow addition of new user");
+ 	if("3, allow edit of user");
+ 	if("4, delete user");
+ 	if("5, Change passowrd");
+ 	
+ 	
+	return 0;
+}
